1961822a80685a8fbd0c9e8f47ac432e90a7712d
rootspyro <root.spyro@gmail.com>
2023-01-09 02:22:53 -0400

    feat: Init project documentation and env variables

---
+++ README.md
@@ -0,0 +1,11 @@
+# Wa.me Url Generator API
+Restful API to dynamically generate whatsapp api urls with a custom message.
+
+## Technologies
+
+## Install and Run the Project
+
+## Environment Variables
+```shell
+PORT=3000 # Example Port Number
+```
--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+	"os"
 	"wsurl-creator-api/handlers"
 	"wsurl-creator-api/services"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main(){
 	
 	r := gin.Default()
 
-	//setup services
+	// Load env variables from .env
+	godotenv.Load()
+
+	// Setup services
 	wsServices := services.NewWsServices()
 	
 	r.GET("/ping", handlers.NewGetPingHandler().Handle)
 	r.POST("/ws/url", handlers.NewGenUrlHandler(wsServices).Handle)
 
-	r.Run()
+	r.Run(":" + os.Getenv("PORT"))
 }
